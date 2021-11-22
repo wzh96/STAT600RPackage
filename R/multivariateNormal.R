bfb1011d4cbd3b7776ea133b708f857f50f1cc6f
wzh96 <wzh96@tamu.edu>
2021-11-22 17:09:02 -0600

    Added multivariate normal distribtion generating function.

---
+++ R/multivariateNormal.R
@@ -0,0 +1,16 @@
+multivariateNormal <-
+  function(mu, Sigma, tol=1e-6)
+  {
+    n <- 1
+    p <- length(mu)
+    if(!all(dim(Sigma) == c(p,p))) stop("incompatible arguments")
+    eS <- eigen(Sigma, symmetric = TRUE)
+    ev <- eS$values
+    if(!all(ev >= -tol*abs(ev[1L]))) stop("'Sigma' is not positive definite")
+    X <- matrix(rnorm(p * n), n)
+    X <- drop(mu) + eS$vectors %*% diag(sqrt(pmax(ev, 0)), p) %*% t(X)
+    nm <- names(mu)
+    if(is.null(nm) && !is.null(dn <- dimnames(Sigma))) nm <- dn[[1L]]
+    dimnames(X) <- list(nm, NULL)
+    if(n == 1) drop(X) else t(X)
+  }
